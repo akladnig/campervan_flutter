42e3c10c932c83ef5fcaaffd15e1ecd93b8e3183
akladnig <adrian@kladnig.com>
2025-05-06 21:00:39 +1000

    updated variable name

--- lib/src/common_widgets/info_card.dart
+++ lib/src/common_widgets/info_card.dart
@@ -1,31 +1,13 @@
 import 'package:campervan/src/common_widgets/base_card.dart';
 import 'package:campervan/src/constants/app_sizes.dart';
 import 'package:campervan/src/constants/theme.dart';
+import 'package:campervan/src/features/arduino/arduino_model.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
-enum InfoType {
-  temperature(
-    label: 'temperature',
-    units: '°C',
-    icon: Icon(FontAwesomeIcons.temperatureThreeQuarters, size: Sizes.iconFA),
-  ),
-  voltage(label: 'voltage', units: 'V', icon: Icon(Icons.battery_std, size: Sizes.icon)),
-  amperage(label: 'amperage', units: 'A', icon: Icon(Icons.battery_std, size: Sizes.icon)),
-  percentage(label: 'percentage', units: '%', icon: Icon(Icons.percent, size: Sizes.icon)),
-  kwH(label: 'kwH', units: 'kwH', icon: Icon(Icons.bolt, size: Sizes.icon));
-
-  final String label;
-  final String units;
-  final Icon icon;
-
-  const InfoType({required this.label, required this.units, required this.icon});
-}
 
 class InfoCard extends BaseCard {
   final double value;
   final String description;
-  final InfoType infoType;
+  final Characteristics infoType;
 
   InfoCard({
     super.cardStyle,
@@ -66,5 +48,5 @@ class TemperatureCard extends InfoCard {
     required this.temperature,
     required super.description,
     super.key,
-  }) : super(infoType: InfoType.temperature, value: temperature);
+  }) : super(infoType: Characteristics.temperature, value: temperature);
 }
