c8b6d281846cb465089036dacec5c32f7d7eef70
akladnig <adrian@kladnig.com>
2025-05-07 07:17:40 +1000

    info is now pulled directly from the deviceMap provider

--- lib/src/common_widgets/info_card.dart
+++ lib/src/common_widgets/info_card.dart
@@ -3,50 +3,48 @@ import 'package:campervan/src/constants/app_sizes.dart';
 import 'package:campervan/src/constants/theme.dart';
 import 'package:campervan/src/features/arduino/arduino_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class InfoCard extends BaseCard {
-  final double value;
-  final String description;
-  final Characteristics infoType;
+class InfoCard extends ConsumerWidget {
+  final Devices device;
+  final bool borderTop;
+  final bool borderLeft;
+  final bool borderRight;
+  final bool borderBottom;
 
-  InfoCard({
-    super.cardStyle,
-    super.cardType,
-    super.borderTop,
-    super.borderLeft,
-    super.borderRight,
-    super.borderBottom,
-    required this.infoType,
-    required this.value,
-    required this.description,
+  const InfoCard({
+    required this.device,
+    this.borderTop = true,
+    this.borderLeft = true,
+    this.borderRight = true,
+    this.borderBottom = true,
     super.key,
-  }) : super(
-         child: Row(
-           children: [
-             infoType.icon,
-             gapWMED,
-             Column(
-               mainAxisAlignment: MainAxisAlignment.center,
-               crossAxisAlignment: CrossAxisAlignment.start,
-               children: [Text('${value.toString()} ${infoType.units}', style: TextStyles.h1), Text(description)],
-             ),
-           ],
-         ),
-       );
-}
+  });
 
-class TemperatureCard extends InfoCard {
-  final double temperature;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    // TODO error checking on device
+    final deviceMap = ref.read(deviceMapProvider);
+    final characteristic = deviceMap[device]!.keys.first;
+    final value = deviceMap[device]!.values.first;
 
-  TemperatureCard({
-    super.cardStyle = CardStyle.outlined,
-    super.cardType,
-    super.borderTop,
-    super.borderLeft = true,
-    super.borderRight = true,
-    super.borderBottom = true,
-    required this.temperature,
-    required super.description,
-    super.key,
-  }) : super(infoType: Characteristics.temperature, value: temperature);
+    return BaseCard(
+      borderTop: borderTop,
+      borderRight: borderRight,
+      borderBottom: borderBottom,
+      borderLeft: borderLeft,
+
+      child: Row(
+        children: [
+          characteristic.icon,
+          gapWMED,
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [Text('${value.toString()} ${characteristic.units}', style: TextStyles.h1), Text(device.label)],
+          ),
+        ],
+      ),
+    );
+  }
 }
