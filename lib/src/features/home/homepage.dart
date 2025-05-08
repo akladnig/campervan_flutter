dd449dea2460a6789ada97c1978e9d6a6b722632
akladnig <adrian@kladnig.com>
2025-05-08 13:12:11 +1000

    implemented LayoutGrid and created Widget OutlinedBox

--- lib/src/features/home/homepage.dart
+++ lib/src/features/home/homepage.dart
@@ -1,7 +1,11 @@
+import 'package:campervan/src/common_widgets/base_card.dart';
 import 'package:campervan/src/common_widgets/info_card.dart';
 import 'package:campervan/src/common_widgets/switch_card.dart';
+import 'package:campervan/src/constants/app_sizes.dart';
+import 'package:campervan/src/constants/theme.dart';
 import 'package:campervan/src/features/arduino/arduino_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_layout_grid/flutter_layout_grid.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:campervan/src/common_widgets/app_bar.dart';
 import 'package:campervan/src/constants/theme_provider.dart';
@@ -21,7 +25,6 @@ class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
     theme = ref.watch(themeProvider).themeMode;
-    ref.watch(settingsProvider);
 
     return Scaffold(
       appBar: const PreferredSize(
@@ -34,19 +37,26 @@ class _HomePageState extends ConsumerState<HomePage> {
           // width: Breakpoint.tablet,
           padding: const EdgeInsets.all(20.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Row(
-                children: [
-                  InfoCard(device: D.ambientTemperature, border: 'tl'),
-                  InfoCard(device: D.battery2in, border: 'tr'),
-                ],
-              ),
+              FittedBox(
+                child: OutlinedBox(
+                  child: LayoutGrid(
+                    areas: '''
+                      at  b2i
+                      bbt b2o
+                    ''',
+                    rowSizes: [auto, auto],
+                    columnSizes: [auto, auto],
+                    children: [
+                      InfoCard(device: D.ambientTemperature, border: 'tl').inGridArea('at'),
+                      InfoCard(device: D.battery2in, border: 'tr').inGridArea('b2i'),
 
-              Row(
-                children: [
-                  InfoCard(device: D.batteryBoxTemperature, border: 'bl'),
-                  InfoCard(device: D.battery2out, border: 'br'),
-                ],
+                      InfoCard(device: D.batteryBoxTemperature, border: 'bl').inGridArea('bbt'),
+                      InfoCard(device: D.battery2out, border: 'br').inGridArea('b2o'),
+                    ],
+                  ),
+                ),
               ),
               SwitchCard(title: 'Battery 2', iconData: Icons.battery_std),
             ],
@@ -54,5 +64,25 @@ class _HomePageState extends ConsumerState<HomePage> {
         ),
       ),
     );
+  }
+}
+
+class OutlinedBox extends ConsumerWidget {
+  const OutlinedBox({required this.child, super.key});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cardStyle = ref.watch(settingsProvider).cardStyle;
+
+    final boxDecoration =
+        cardStyle == CardStyle.outlined
+            ? BoxDecoration(
+              border: Border.all(color: primaryColour, width: Sizes.borderWidth),
+              borderRadius: BorderRadius.circular(Sizes.medium + Sizes.borderWidth),
+            )
+            : null;
+
+    return Container(margin: EdgeInsets.all(Sizes.margin), decoration: boxDecoration, child: child);
   }
 }
