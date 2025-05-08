0f8ac11f23e7fd93af928a33db6330b94192700f
akladnig <adrian@kladnig.com>
2025-05-08 13:10:50 +1000

    added cardStyle

--- lib/src/repository/objectbox/objectbox_settings_model.dart
+++ lib/src/repository/objectbox/objectbox_settings_model.dart
@@ -1,3 +1,4 @@
+import 'package:campervan/src/common_widgets/base_card.dart';
 import 'package:flutter/material.dart';
 import 'package:campervan/src/features/settings/settings_model.dart';
 import 'package:campervan/src/utils/logging_provider.dart';
@@ -9,6 +10,7 @@ final class ObxSettingsModel {
   ObxSettingsModel({
     this.id = 0,
     this.themeMode = ThemeMode.system,
+    this.cardStyle = CardStyle.outlined,
     this.loggingLevel = Level.debug,
     this.loggingPrinterDetail = PrinterDetail.high,
     this.methodCount = 1,
@@ -18,17 +20,27 @@ final class ObxSettingsModel {
   @Transient()
   ThemeMode themeMode;
   @Transient()
+  CardStyle cardStyle;
+  @Transient()
   Level loggingLevel;
   @Transient()
   PrinterDetail loggingPrinterDetail;
   int methodCount;
-  @Transient()
+
   int get obxThemeMode {
     return themeMode.index;
   }
 
   set obxThemeMode(int index) {
     themeMode = ThemeMode.values[index];
+  }
+
+  int get obxCardStyle {
+    return cardStyle.index;
+  }
+
+  set obxCardStyle(int index) {
+    cardStyle = CardStyle.values[index];
   }
 
   int get obxLoggingLevel {
@@ -50,6 +62,7 @@ final class ObxSettingsModel {
   SettingsModel toSettingsModel() {
     return SettingsModel(
       themeMode: themeMode,
+      cardStyle: cardStyle,
       loggingLevel: loggingLevel,
       loggingPrinterDetail: loggingPrinterDetail,
       methodCount: methodCount,
