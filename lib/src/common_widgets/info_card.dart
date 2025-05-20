2e4714435d5c20cbd15cb69a4d64435adeae3d4a
akladnig <adrian@kladnig.com>
2025-05-20 15:42:17 +1000

    first working attempt with hard coded values

--- lib/src/common_widgets/info_card.dart
+++ lib/src/common_widgets/info_card.dart
@@ -1,7 +1,9 @@
+import 'package:campervan/src/common_widgets/async_value_widget.dart';
 import 'package:campervan/src/common_widgets/base_card.dart';
 import 'package:campervan/src/constants/app_sizes.dart';
 import 'package:campervan/src/constants/theme.dart';
 import 'package:campervan/src/features/arduino/arduino_model.dart';
+import 'package:campervan/src/features/arduino/arduino_web_socket_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,9 +18,7 @@ class InfoCard extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     // TODO error checking on device
-    final deviceMap = ref.read(deviceMapProvider);
-    final characteristic = deviceMap[device]!.keys.first;
-    final value = deviceMap[device]!.values.first;
+    final characteristic = C.temperature;
 
     bool borderTop = border.contains('t');
     bool borderRight = border.contains('r');
@@ -38,10 +38,27 @@ class InfoCard extends ConsumerWidget {
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
-            children: [Text('${value.toString()} ${characteristic.units}', style: TextStyles.h1), Text(device.label)],
+            children: [CharacteristicValueWidget(device: device), Text(device.label)],
           ),
         ],
       ),
+    );
+  }
+}
+
+class CharacteristicValueWidget extends ConsumerWidget {
+  final Devices device;
+
+  const CharacteristicValueWidget({required this.device, super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final deviceMap = ref.watch(deviceCharacteristicsProvider);
+    debugPrint('info: ${deviceMap.toString()}');
+
+    return AsyncValueWidget(
+      value: deviceMap,
+      data: (data) => Text('${data.deviceMap[device]} ${C.temperature.units}', style: TextStyles.h1),
     );
   }
 }
