f5d6f10f515dc5e856260c69c188229d56b470bd
akladnig <adrian@kladnig.com>
2025-05-20 12:13:52 +1000

    moved to layout grid

--- lib/src/features/home/homepage.dart
+++ lib/src/features/home/homepage.dart
@@ -25,6 +25,8 @@ class _HomePageState extends ConsumerState<HomePage> {
   @override
   Widget build(BuildContext context) {
     theme = ref.watch(themeProvider).themeMode;
+    // ref.watch(arduinoWebSocketProvider);
+    // final deviceCharacteristics = ref.watch(deviceCharacteristicsProvider);
 
     return Scaffold(
       appBar: const PreferredSize(
@@ -34,31 +36,30 @@ class _HomePageState extends ConsumerState<HomePage> {
       body: SafeArea(
         child: Container(
           width: 1000,
+          height: 800,
           // width: Breakpoint.tablet,
           padding: const EdgeInsets.all(20.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              FittedBox(
-                child: OutlinedBox(
-                  child: LayoutGrid(
-                    areas: '''
+              OutlinedBox(
+                child: LayoutGrid(
+                  areas: '''
                       at  b2i
                       bbt b2o
                     ''',
-                    rowSizes: [auto, auto],
-                    columnSizes: [auto, auto],
-                    children: [
-                      InfoCard(device: D.ambientTemperature, border: 'tl').inGridArea('at'),
-                      InfoCard(device: D.battery2in, border: 'tr').inGridArea('b2i'),
+                  rowSizes: [auto, auto],
+                  columnSizes: [auto, auto],
+                  children: [
+                    InfoCard(device: D.ambientTemperature, border: 'tl').inGridArea('at'),
+                    InfoCard(device: D.battery2in, border: 'tr').inGridArea('b2i'),
 
-                      InfoCard(device: D.batteryBoxTemperature, border: 'bl').inGridArea('bbt'),
-                      InfoCard(device: D.battery2out, border: 'br').inGridArea('b2o'),
-                    ],
-                  ),
+                    InfoCard(device: D.batteryBoxTemperature, border: 'bl').inGridArea('bbt'),
+                    InfoCard(device: D.battery2out, border: 'br').inGridArea('b2o'),
+                  ],
                 ),
               ),
-              SwitchCard(title: 'Battery 2', iconData: Icons.battery_std),
+              OutlinedBox(child: SwitchCard(title: 'Battery 2', iconData: Icons.battery_std)),
             ],
           ),
         ),
@@ -83,6 +84,6 @@ class OutlinedBox extends ConsumerWidget {
             )
             : null;
 
-    return Container(margin: EdgeInsets.all(Sizes.margin), decoration: boxDecoration, child: child);
+    return FittedBox(child: Container(margin: EdgeInsets.all(Sizes.margin), decoration: boxDecoration, child: child));
   }
 }
